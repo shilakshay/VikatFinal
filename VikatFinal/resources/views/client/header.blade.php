6f30f9757d92ee56a8098abfe06fda541a404412
Akshay Shil <shilakshay19@gmail.com>
2019-07-22 23:06:41 +0530

    First

---
+++ VikatFinal/resources/views/client/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <div id="header-container">
+        <a href="#" class="logo"><img src="/images/logo.png"></a>
+
+        <nav>
+            <ul>
+                <li><a href="index.html" class="active">Home</a></li>
+                <li><a href="products.html">Products</a></li>
+                <li><a href="aboutus.html">About Us</a></li>
+                <li><a href="contactus.html">Contact Us</a></li>
+            </ul>
+        </nav>
+
+        <img class="searchicon" src="/images/search.png">
+
+        <div class="divider">
+
+        </div>
+
+        <button class="cartbutton">
+            <img src="/images/Bag.png">
+            Cart
+        </button>
+    </div>
+
+</header>
